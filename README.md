f9d47e5ea027c3a559fa3295272cbf2be3971116
adrmrt <adrian.e.martinez.ayuso@gmail.com>
2026-02-19 09:42:56 +0100

    Added enis' script

---
+++ README.md
@@ -0,0 +1,9 @@
+# BA Image Downloader
+
+https://www.inaturalist.org/pages/developers
+
+## Example
+
+https://www.inaturalist.org/observations?photo_license=cc0&taxon_id=121196
+
+https://api.inaturalist.org/v1/observations
---
+++ inat_download.py
@@ -0,0 +1,84 @@
+from typing import Any
+from pyparsing import Path
+import requests
+import os
+import re
+import json
+import time
+
+API_URL = "https://api.inaturalist.org/v1/observations"
+DEFAULT_MAX_IMAGES = 150
+PER_PAGE = 200
+LICENSE = "cc0"
+
+BASE_DIR = Path(__file__).resolve().parent
+JSON_PATH = BASE_DIR / "fishes.json"
+OUT_DIR = BASE_DIR / "out"
+
+
+def download_images_for_taxon(name: str, taxon_id: int):
+    out = OUT_DIR / name
+
+    if out.exists():
+        print(f"Skipped (already exists): {name}")
+        return
+
+    out.mkdir(parents=True, exist_ok=True)
+
+    page = 1
+    downloaded = 0
+
+    while downloaded < DEFAULT_MAX_IMAGES:
+        params = {
+            "taxon_id": taxon_id,
+            "quality_grade": "research",
+            "photos": "true",
+            "license": LICENSE,
+            "per_page": PER_PAGE,
+            "page": page,
+        }
+
+        r = requests.get(API_URL, params=params, timeout=15)
+        r.raise_for_status()
+        results = r.json().get("results", [])
+        if not results:
+            break
+
+        for obs in results:
+            for photo in obs.get("photos", []):
+                if downloaded >= DEFAULT_MAX_IMAGES:
+                    break
+
+                url = photo.get("url")
+                if not url:
+                    continue
+
+                img_url = url.replace("square", "large")
+                ext = img_url.split(".")[-1].split("?")[0] or "jpg"
+                path = out / f"img_{downloaded:03}.{ext}"
+
+                try:
+                    img = requests.get(img_url, timeout=15)
+                    img.raise_for_status()
+                except requests.RequestException:
+                    continue
+
+                path.write_bytes(img.content)
+                downloaded += 1
+                print(f"✔ {name}: {downloaded}")
+
+        page += 1
+        time.sleep(0.5)
+
+    print(f"Finished with {name}: {downloaded} Images\n")
+
+
+def main():
+    fishes = json.loads(JSON_PATH.read_text(encoding="utf-8"))
+    for fish in fishes:
+        print(f"\nStart download for: {fish['name']}")
+        download_images_for_taxon(fish["name"], int(fish["taxon_id"]))
+
+
+if __name__ == "__main__":
+    main()
